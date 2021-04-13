dfd4743134c94af4e69b0cb30249925d52602b77
takemura hayato <you@example.com>
2021-04-13 18:23:19 +0900

    'about削除'

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,7 +3,6 @@ from todo_list import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
     path('delete/<list_id>', views.delete, name="delete"),
     path('uncomplete/<list_id>', views.uncomplete, name="uncomplete"),
     path('complete/<list_id>', views.complete, name="complete"),
--- todo_list/views.py
+++ todo_list/views.py
@@ -17,11 +17,6 @@ def home(request):
         all_items = List.objects.all
         return render(request, 'home.html', {'all_items': all_items})
 
-
-def about(request):
-    context = {'first_name': 'Harry', 'last_name': 'Potter'}
-    return render(request, 'about.html', context)
-
 def delete(request, list_id):
     item = List.objects.get(pk=list_id)
     item.delete()
